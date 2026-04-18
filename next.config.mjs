d987541667c739170400c62d8b02ad90df9ed82a
Ancestor <malik@Agbedeyis-MacBook-Pro.local>
2026-04-18 04:27:40 +0100

    fix: bypass typescript build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**"
+      }
+    ]
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb", 
+    },
+  },
+  
   typescript: {
     ignoreBuildErrors: true,
   },
